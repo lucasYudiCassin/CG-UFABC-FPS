b45de3f58788e4d61b9c2644a362b855a53b4998
Lucas Cassin <lucas.yudi@aluno.ufabc.edu.br>
2021-11-16 19:05:07 -0300

    Changed files

--- assets/depth.vert
+++ assets/depth.vert
@@ -2,6 +2,7 @@
 
 layout(location = 0) in vec3 inPosition;
 
+uniform vec4 color;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projMatrix;
@@ -12,7 +13,7 @@ void main() {
   vec4 posEyeSpace = viewMatrix * modelMatrix * vec4(inPosition, 1);
 
   float i = 1.0 - (-posEyeSpace.z / 3.0);
-  fragColor = vec4(i, i, i, 1);
+  fragColor = vec4(i, i, i, 1) * color;
 
   gl_Position = projMatrix * posEyeSpace;
 }
--- openglwindow.hpp
+++ openglwindow.hpp
@@ -15,41 +15,21 @@ class OpenGLWindow : public abcg::OpenGLWindow {
   void terminateGL() override;
 
  private:
+  GLuint m_program{};
+
   int m_viewportWidth{};
   int m_viewportHeight{};
 
   Model m_model;
   int m_trianglesToDraw{};
 
-  TrackBall m_trackBallModel;
-  TrackBall m_trackBallLight;
+  TrackBall m_trackBall;
   float m_zoom{};
 
   glm::mat4 m_modelMatrix{1.0f};
   glm::mat4 m_viewMatrix{1.0f};
   glm::mat4 m_projMatrix{1.0f};
 
-  // Shaders
-  std::vector<const char*> m_shaderNames{"texture", "blinnphong", "phong",
-                                         "gouraud", "normal",     "depth"};
-  std::vector<GLuint> m_programs;
-  int m_currentProgramIndex{};
-
-  // Mapping mode
-  // 0: triplanar; 1: cylindrical; 2: spherical; 3: from mesh
-  int m_mappingMode{};
-
-  // Light and material properties
-  glm::vec4 m_lightDir{-1.0f, -1.0f, -1.0f, 0.0f};
-  glm::vec4 m_Ia{1.0f};
-  glm::vec4 m_Id{1.0f};
-  glm::vec4 m_Is{1.0f};
-  glm::vec4 m_Ka;
-  glm::vec4 m_Kd;
-  glm::vec4 m_Ks;
-  float m_shininess{};
-
-  void loadModel(std::string_view path);
   void update();
 };
 
