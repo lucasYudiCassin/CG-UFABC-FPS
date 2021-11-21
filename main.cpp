d7c20a1ba042bf06328c720c7f239234efa80e9c
Fernando Kenji <kenjifernandoo@gmail.com>
2021-11-21 20:51:43 -0300

    fix: window title

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     auto window{std::make_unique<OpenGLWindow>()};
     window->setOpenGLSettings({.samples = 4});
     window->setWindowSettings(
-        {.width = 600, .height = 600, .title = "Model Viewer (version 1)"});
+        {.width = 600, .height = 600, .title = "FPS Aim trainer"});
 
     app.run(std::move(window));
   } catch (const abcg::Exception &exception) {
