6aa22f4fa5c55bc2fbb2e479fac572f1eef1ccd8
Lucas Cassin <lucas.yudi@aluno.ufabc.edu.br>
2021-11-21 21:41:55 -0300

    Change room scale

--- openglwindow.cpp
+++ openglwindow.cpp
@@ -181,7 +181,7 @@ void OpenGLWindow::renderRoom() {
   // Rotation angle
   const auto angle = glm::radians(90.0f);
   modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.0f, 1.0f));
-  modelMatrix = glm::scale(modelMatrix, glm::vec3(2.0f, 2.0f, 2.0f));
+  modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 2.0f, 2.0f));
   modelMatrix = glm::rotate(modelMatrix, -angle, m_camera.m_up);
 
   // glm::mat4 rotateMatrix = glm::rotate(glm::mat4(1.0f), angle, m_axis);
